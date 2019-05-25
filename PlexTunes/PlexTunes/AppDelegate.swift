6a0f20ead10a42a1da5bf87bb7fd9da6df2dc6c1
DrEVILish <ryan.j.freeman@me.com>
2019-05-25 20:45:18 +0100

    initial

---
+++ PlexTunes/PlexTunes/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  PlexTunes
+//
+//  Created by Ryan Freeman on 25/05/2019.
+//  Copyright © 2019 DrEVILish. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
